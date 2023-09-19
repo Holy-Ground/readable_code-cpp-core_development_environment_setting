92f46da26d7808daadba500b3d72ee64ba1fbe47
movingChurch <seong9115@gmail.com>
2023-09-19 13:43:11 +0900

    feat/Added main function printing str

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 HolyGround, All Rights Reserved.
+// Authors: EunSung Yang
+
+#include <cstdint>
+#include <iostream>
+
+auto main() -> int32_t {
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
