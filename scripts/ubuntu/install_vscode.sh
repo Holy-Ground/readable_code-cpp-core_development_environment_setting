98135c4a6da5e942e623d0ddc19614febc109341
movingChurch <seong9115@gmail.com>
2023-10-27 17:15:41 +0900

    Changed scripts

--- scripts/ubuntu/install_vscode.sh
+++ scripts/ubuntu/install_vscode.sh
@@ -7,7 +7,7 @@ sudo apt -y install software-properties-common apt-transport-https wget
 # Install VSCode
 echo "Installing VSCode..."
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install -y code
 
 # Verify installed versions
