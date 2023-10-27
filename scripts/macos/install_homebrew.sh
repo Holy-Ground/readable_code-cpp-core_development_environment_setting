fc3c9e0dccf406d5bfbb8cafe7281cc888dedc09
movingChurch <seong9115@gmail.com>
2023-10-27 16:54:26 +0900

    Removed a wrong command

--- scripts/macos/install_homebrew.sh
+++ scripts/macos/install_homebrew.sh
@@ -4,6 +4,4 @@
 if ! command -v brew &> /dev/null; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/undo/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
